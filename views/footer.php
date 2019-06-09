2502db50c8ad09483131ace892e8c7902ac6cbf8
RaduMugurBogdan <radumugur1997@gmail.com>
2019-06-09 22:12:53 +0300

    done with first page of site

---
+++ views/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer-container">
+        <div class="container">
+            <a href="#">
+                <img src="../assets/images/logo-white.svg" class="logo" alt="logo">
+            </a>
+            <p class="address">Melrose Place, 9012123 <br>USA</p>
+            <ul class="footer-links">
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ views/header.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="logo-body">
+        <img src="../assets/images/logo.png" alt="23andfour logo" class="logo">
+    </div>
+    <nav>
+        <a href='#' class="hide-desktop">
+            <img src="../assets/images/ham.svg" alt="toggle menu" class="menu" id="menu">
+        </a>
+        <ul class="show-desktop hide-mobile" id="nav">
+            <li id="exit" class="exit-btn hide-desktop"><img src="../assets/images/exit.svg" alt="exit menu"></li>
+            <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+            <li><a href="#"><i class="fas fa-address-card"></i> Contact</a></li>
+            <li><a href="#"><i class="fas fa-shopping-cart"></i> Shop</a></li>
+            <li><a href="#"><i class="fas fa-user"></i> Account</a></li>
+        </ul>
+    </nav>
+</header>
